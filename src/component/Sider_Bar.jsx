6528347c0d0c55b231deb2cde15feaa5e161ff82
jatinpanchal1o1 <jatin1o1panchal@gmail.com>
2022-08-23 21:17:42 +0530

    update-navRespone

--- src/component/Sider_Bar.jsx
+++ src/component/Sider_Bar.jsx
@@ -29,19 +29,28 @@ import User_icon from "../assets/Dazzie_Icons/user.svg";
 import Setting_icon from "../assets/Dazzie_Icons/seettings.svg";
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Sider_Bar = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [state, setState] = useState(false);
   const [styled, setStyled] = useState({
     display: "none",
   });
 
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+  }, []);
+
   const handler = () => {
-    state === false ? setState(true) : setState(false);
+    setState(!state);
     state === true
-      ? setStyled({ display: "block" })
-      : setStyled({ display: "none" });
+      ? setStyled({ display: "none" })
+      : setStyled({ display: "block" });
     console.log(styled);
     console.log(state);
   };
@@ -55,55 +64,60 @@ const Sider_Bar = () => {
   //Javascript split method to get the name of the path in array
   const splitLocation = pathname.split("/");
 
+  // {
+  //   `Sidebar ${!state ? "expandSidbar" : ""}`;
+  // }
   return (
     <div className="container">
+      {(state || screenWidth > 899) && (
+        <div className="Sidebar">
+          <div className="topSide">
+            <ul className="topList">
+              <li className={splitLocation[1] === "" ? "active" : ""}>
+                <NavLink to="/">
+                  <AiOutlineAppstore style={style} />
+                </NavLink>
+              </li>
+              <li className={splitLocation[1] === "order" ? "active" : ""}>
+                <NavLink to="order">
+                  <img src={Order_icon} alt="order_icon" />
+                </NavLink>
+              </li>
+              <li className={splitLocation[1] === "customer" ? "active" : ""}>
+                <NavLink to="customer">
+                  <img src={Customer_icon} alt="customer_icon" />
+                </NavLink>
+              </li>
+              <li className={splitLocation[1] === "package" ? "active" : ""}>
+                <NavLink to="package">
+                  <img src={Package_icon} alt="package_icon" />
+                </NavLink>
+              </li>
+              <li className={splitLocation[1] === "coupons" ? "active" : ""}>
+                <NavLink to="coupons">
+                  <img src={Coupon_icon} alt="coupon_icon" />
+                </NavLink>
+              </li>
+              <li className={splitLocation[1] === "user" ? "active" : ""}>
+                <NavLink to="user">
+                  <img src={User_icon} alt="user_icon" />
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="bottomSide">
+            <ul className="bottomList">
+              <li className={splitLocation[1] === "setting" ? "active" : ""}>
+                <NavLink to="/setting">
+                  <img src={Setting_icon} alt="setting_icon" />
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
       <div className="aiMenu">
         <AiOutlineMenu style={style} onClick={handler} />
-      </div>
-      <div id="Sidebar">
-        <div className="topSide">
-          <ul className="topList">
-            <li className={splitLocation[1] === "" ? "active" : ""}>
-              <NavLink to="/">
-                <AiOutlineAppstore style={style} />
-              </NavLink>
-            </li>
-            <li className={splitLocation[1] === "order" ? "active" : ""}>
-              <NavLink to="order">
-                <img src={Order_icon} alt="order_icon" />
-              </NavLink>
-            </li>
-            <li className={splitLocation[1] === "customer" ? "active" : ""}>
-              <NavLink to="customer">
-                <img src={Customer_icon} alt="customer_icon" />
-              </NavLink>
-            </li>
-            <li className={splitLocation[1] === "package" ? "active" : ""}>
-              <NavLink to="package">
-                <img src={Package_icon} alt="package_icon" />
-              </NavLink>
-            </li>
-            <li className={splitLocation[1] === "coupons" ? "active" : ""}>
-              <NavLink to="coupons">
-                <img src={Coupon_icon} alt="coupon_icon" />
-              </NavLink>
-            </li>
-            <li className={splitLocation[1] === "user" ? "active" : ""}>
-              <NavLink to="user">
-                <img src={User_icon} alt="user_icon" />
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className="bottomSide">
-          <ul className="bottomList">
-            <li className={splitLocation[1] === "setting" ? "active" : ""}>
-              <NavLink to="/setting">
-                <img src={Setting_icon} alt="setting_icon" />
-              </NavLink>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   );
